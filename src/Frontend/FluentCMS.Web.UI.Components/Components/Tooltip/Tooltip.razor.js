c63d49cdbc1f4051d34bb71087d904c56e33951e
Hadi Ahmadi <TheHadiAhmadi@gmail.com>
2025-09-20 20:36:22 +0430

    Add Bootsrtap theme (#2360)
    * Working on bootstrap theme
    
    * fix some components
    
    * update styles
    
    * fix some bugs
    
    * update default and portfolio templates
    
    * Add inline editor boorstrap styles
    
    * update edit layout
    
    * Add bootstrap based blocks
    
    * Update default template
    
    * update dark mode color
    
    * update content list plugin styles
    
    * update portfolio template
    
    * update portfolio template layout
    
    * fix contact us page
    
    * update
    
    * update
    
    * fix style issues

--- src/Frontend/FluentCMS.Web.UI.Components/Components/Tooltip/Tooltip.razor.js
+++ src/Frontend/FluentCMS.Web.UI.Components/Components/Tooltip/Tooltip.razor.js
@@ -1,3 +1,6 @@
+import '/_content/FluentCMS.Web.UI.Components/js/floating-ui.core.js'
+import '/_content/FluentCMS.Web.UI.Components/js/floating-ui.dom.js'
+
 const tooltips = new Map();
 
 export function update(dotnet, element, config) {
@@ -8,25 +11,57 @@ export function initialize(dotnet, element, config) {
     dispose(dotnet, element);
 
     const target = element;
-
     if (!element) return;
-
     const trigger = element.previousElementSibling;
 
     const options = {
         placement: config.placement,
-        triggerType: 'hover',
+        middleware: [
+            FloatingUIDOM.offset(6),
+            FloatingUIDOM.flip(), 
+            FloatingUIDOM.shift({padding: 5}), 
+        ]
     };
 
-    const tooltip = new window.Flowbite.default.Tooltip(target, trigger, options);
+    const cleanup = FloatingUIDOM.autoUpdate(trigger, target, () => {
+        FloatingUIDOM.computePosition(trigger, target, options).then(({x, y}) => {
+            Object.assign(element.style, {
+                left: `${x}px`,
+                top: `${y}px`,
+            });
+        });
+    })
+
+    function showTooltip() {
+        Object.assign(element.style, {
+            display: 'block'
+        });
+    } 
+
+    function hideTooltip() {
+        Object.assign(element.style, {
+            display: 'none'
+        });
+    }
+
+    hideTooltip()
+    trigger.addEventListener('mouseenter', showTooltip)
+    trigger.addEventListener('mouseleave', hideTooltip)
+
+    const tooltip = {
+        destroy() {
+            trigger.removeEventListener('mouseenter', showTooltip)
+            trigger.removeEventListener('mouseleave', hideTooltip)
+            cleanup()
+        }
+    }
+
 
     tooltips.set(element, tooltip);
 }
 
 export function dispose(dotnet, element) {
     const tooltip = tooltips.get(element);
-
     tooltip?.destroy();
-
     tooltips.delete(element);
 }
